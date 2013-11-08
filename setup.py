df0cea6f98beaf723b67e7999a13b2d35e9802c3
metagriffin <mg.github@uberdev.org>
2013-11-08 11:03:52 -0500

    added initial python setup env

---
+++ setup.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+# file: $Id$
+# auth: metagriffin <metagriffin@uberdev.org>
+# date: 2013/11/08
+# copy: (C) CopyLoose 2010 UberDev <hardcore@uberdev.org>, No Rights Reserved.
+#------------------------------------------------------------------------------
+
+import os, sys, setuptools
+from setuptools import setup, find_packages
+
+# require python 2.7+
+if sys.hexversion < 0x02070000:
+  raise RuntimeError('This package requires python 2.7 or better')
+
+heredir = os.path.abspath(os.path.dirname(__file__))
+def read(*parts, **kw):
+  try:    return open(os.path.join(heredir, *parts)).read()
+  except: return kw.get('default', '')
+
+test_dependencies = [
+  'nose                 >= 1.3.0',
+  'coverage             >= 3.5.3',
+  ]
+
+dependencies = [
+  'distribute           >= 0.6.24',
+  ]
+
+classifiers = [
+  'Development Status :: 1 - Planning',
+  # 'Development Status :: 2 - Pre-Alpha',
+  # 'Development Status :: 3 - Alpha',
+  # 'Development Status :: 4 - Beta',
+  # 'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Developers',
+  'Operating System :: OS Independent',
+  'Programming Language :: Python',
+  'Topic :: Software Development',
+  'Topic :: Software Development :: Libraries :: Python Modules',
+  'Topic :: Utilities',
+  'License :: OSI Approved :: MIT License',
+  'License :: Public Domain',
+  ]
+
+setup(
+  name                  = 'morph',
+  version               = read('VERSION.txt', default='0.0.1').strip(),
+  description           = 'A collection of routines to help identify and morph objects.',
+  long_description      = read('README.rst'),
+  classifiers           = classifiers,
+  author                = 'metagriffin',
+  author_email          = 'mg.pypi@uberdev.org',
+  url                   = 'http://github.com/metagriffin/morph',
+  keywords              = 'morph string list tuple dict flatten unflatten tolist tobool',
+  packages              = find_packages(),
+  platforms             = ['any'],
+  include_package_data  = True,
+  zip_safe              = True,
+  install_requires      = dependencies,
+  tests_require         = test_dependencies,
+  test_suite            = 'morph',
+  entry_points          = entrypoints,
+  license               = 'MIT (http://opensource.org/licenses/MIT)',
+  )
+
+#------------------------------------------------------------------------------
+# end of $Id$
+#------------------------------------------------------------------------------
